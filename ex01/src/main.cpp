a486adbcea0a1bdba076ba96f4c99295806a3578
lvan-bus <lucienvanbussel@gmail.com>
2023-06-29 15:40:46 +0200

    finished ex02 and created setup for ex03

--- ex01/src/main.cpp
+++ ex01/src/main.cpp
@@ -4,15 +4,13 @@ Zombie	*zombieHorde(int N, std::string name);
 
 int	main(void)
 {
-	uint32_t	N = 8;
 	std::string	name = "zombieName";
 	Zombie		*horde;
+	uint32_t	N = 8;
 
 	horde = zombieHorde(N, name);
-
 	for (uint32_t i = 0; i < N; i++)
 		horde[i].announce();
-
 	delete[] horde;
 	return (0);
 }
---
+++ ex02/src/main.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <iostream>
+
+int main(void) {
+    std::string str = "HI THIS IS BRAIN";
+    std::string *strPTR = &str;
+    std::string &strREF = str;
+
+    std::cout << "The memory address of:" << std::endl;
+    std::cout << "str: " << &str << std::endl;
+    std::cout << "strPTR: " << &strPTR << std::endl;
+    std::cout << "strREF: " << &strREF << std::endl;
+
+    std::cout << "\nThe value of:" << std::endl;
+    std::cout << "str: " << str << std::endl;
+    std::cout << "strPTR: " << strPTR << std::endl;
+    std::cout << "strREF: " << strREF << std::endl;
+    return (0);
+}
